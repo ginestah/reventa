30d09ddd452fbb7b2b632d4bbb364adc79106f6f
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-03-08 16:43:17 -0500

    server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express')
 const db = require("")
-const cors=require('cors')
+const cors = require('cors')
+const bodyParser = require("body-parser")
+const logger = require('morgan')
+const routes = require('./routes')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+app.use('./api', routes)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT,()=>console.log('Listening on port:${PORT}'))
