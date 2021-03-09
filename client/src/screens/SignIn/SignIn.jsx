b4675955e6cebcb8284d14fde081c4a5073b6a2f
Brittany Owen <owen.brittany@gmail.com>
2021-03-09 13:42:08 -0600

    SignIn.jsx and SignUp.jsx basci set up complete

---
+++ client/src/screens/SignIn/SignIn.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import './SignIn.css'
+import { signIn } from '../../services/users'
+import {useHistory} from 'react-router-dom'
+
+const SignIn = (props) => {
+
+  const history = useHistory 
+
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+    isError: false,
+    errorMsg: ''
+  })
+
+  const handleChange = event => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const onSignIn = event => {
+    event.preventDefault()
+
+    const { setUser } = props
+    
+    signIn(form)
+      .then(user => {
+        setUser(user)
+      })
+      .then(() => history.push('/'))
+      .catch(error => {
+        console.error(error)
+        setForm({
+          isError: true,
+          errorMsg: 'Invalid User Credentails',
+          username: '',
+          password: ''
+        })
+      })
+  }
+
+  const renderError = () => {
+    const toggleForm = form.isError ? 'danger' : ''
+    if (form.isError) {
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      )
+    } else {
+      return <button type="submit">Sign In</button>
+    }
+  }
+
+  const { username, password } = form
+
+  return (
+    <div className="form-container">
+      <h3>Sign In</h3>
+      <label>Username</label>
+      <input
+        required
+        type="text"
+        name="username"
+        value={username}
+        placeholder="Enter Username"
+        onChange={handleChange} />
+      <label>Password</label>
+      <input
+        required
+        type="password"
+        name="password"
+        value={password}
+        placeholder="Password"
+        onChange={handleChange} />
+      {renderError()}
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ client/src/screens/SignUp/SignUp.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "./SignUp.css";
+import { signUp, signIn } from "../../services/users";
+import { useHistory } from "react-router-dom";
+
+const SignUp = () => {
+  const history = useHistory();
+
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    isError: false,
+    errorMsg: "",
+  });
+
+  const handleChange = (event) =>
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+
+  const onSignUp = (event) => {
+    event.preventDefault();
+    const { setUser } = props;
+
+    signUp(form)
+      .then(() => signIn(form))
+      .then((user) => setUser(user))
+      .then(() => history.push("/"))
+      .catch((error) => {
+        console.error(error);
+        setForm({
+          email: "",
+          password: "",
+          passwordConfirmation: "",
+          isError: "",
+          errorMsg: "Sign Up Details Invalid",
+        });
+      });
+  };
+
+  const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
+    if (form.isError) {
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      );
+    } else {
+      return <button type="submit">Sign Up</button>;
+    }
+  };
+
+  const { email, username, password, passwordConfirmation } = form;
+
+  return (
+    <div className="form-container">
+      <h3>Sign Up</h3>
+      <form onSubmit={onSignUp}>
+        <label>Username</label>
+        <input
+          required
+          type="text"
+          name="username"
+          value={username}
+          placeholder="Enter Username"
+          onChange={handleChange}
+        />
+        <label>Email Address</label>
+        <input
+          required
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Enter email"
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          required
+          type="password"
+          name="password"
+          value={password}
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <label>Confirm Password</label>
+        <input
+          required
+          type="password"
+          name="passwordConfirmation"
+          value={passwordConfirmation}
+          placeholder="Confirm Password"
+          onChange={handleChange}
+        />
+        {renderError()}
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
