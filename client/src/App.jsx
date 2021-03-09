8d390a1bfc9914ba5f4d9c7877739b80668e48f2
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-03-09 14:17:59 -0500

    done with services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./screens/Home/Home";
 import Products from "./screens/Products/Products";
 import ProductCreate from "./screens/ProductCreate/ProductCreate";
 import ProductEdit from "./screens/ProductEdit/ProductEdit";
-import ProductDetail from "./screens/ProductDetail/ProductDetail";
+// import ProductDetail from "./screens/ProductDetail/ProductDetail";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { verifyUser } from "./services/users";
 import SignUp from "./screens/SignUp/SignUp";
@@ -50,9 +50,9 @@ function App() {
         <Route exact path="/products/:id/edit">
           {user ? <ProductEdit user={user} /> : <Redirect to="/" />}
         </Route>
-        <Route exact path="/products/:id">
+        {/* <Route exact path="/products/:id">
           <ProductDetail user={user} />
-        </Route>
+        </Route> */}
       </Switch>
     </div>
   );
