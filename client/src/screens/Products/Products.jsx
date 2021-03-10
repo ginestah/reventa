424cb0f9a90b49585d537a6c501f30ca6f30fb93
Zoe Li <59024841+Zoe-creator@users.noreply.github.com>
2021-03-10 09:32:49 -0500

    checkbox could not function when all or few are checked, css products… (#20)
    * checkbox could not function when all or few are checked, css products page
    
    * layout css
    
    * zoe
    
    Co-authored-by: lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>

--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -1,3 +1,4 @@
+
 import { useState, useEffect } from "react";
 import "./Products.css";
 import Product from "../../components/Product/Product";
@@ -6,10 +7,13 @@ import Layout from "../../components/shared/Layout/Layout";
 import { getProducts } from "../../services/products";
 import Checkbox from "../../components/Checkbox/Checkbox";
 
+
 const Products = (props) => {
   const [allProducts, setAllProducts] = useState([]);
   const [queriedProducts, setQueriedProducts] = useState([]);
-  const [checked, setChecked] = useState(false);
+  const [isChecked, setChecked] = useState(false);
+  const [options, setOptions] = useState([])
+  const [selectedProduct, setSelectedProduct] = useState([])
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -20,38 +24,80 @@ const Products = (props) => {
     fetchProducts();
   }, []);
 
-  // const handleClick = (e) => {
-  //   setChecked(!checked)
-  //   if (checked===true) {
-  //     const newQueriesProducts = allProducts.filter(product =>
-  //       product.name.toLowerCase().includes(e.target.value.toLowerCase()) )
-  //     setQueriedProducts(newQueriesProducts)
-  //   }
+
+
+
+  const handleChange = (e) => {
+    console.log(e.action)
+    // setChecked(!isChecked)
+    // console.log(isChecked)
+    // console.log(checked)
+    // console.log(e.target.value)
+    const value = e.target.type === 'checkbox' && e.target.checked
+    // console.log(value)
+    if (value) {
+
+      setOptions([...options, e.target.value])
+      console.log(options)
+      //   console.log(options)
+      // }
+      // else {
+      //   const remove = e.target.value
+      //   const indexofRemove = options.findIndex((option) => (option === remove))
+      //   console.log(indexofRemove)
+      //   options.splice(indexofRemove,1,remove)
+      //   setOptions([...options])
+      const newQueriesProducts = allProducts.filter(product =>
+        product.name.toLowerCase().includes(e.target.value.toLowerCase())
+
+      )
+      setQueriedProducts(newQueriesProducts)
+
+    } else {
+      let findNum = options.findIndex(option => option === e.target.value)
+      const removeOne = (options[findNum])
+
+      options.splice(findNum, 1, removeOne)
+
+      setQueriedProducts(allProducts)
+    }
+
+
+
+
+    // setSelectedProduct([...selectedProduct,newQueriesProducts])
+  }
+
+
+  //   const checkSubmit = (e) => {
+  //     let checkedArray = [];
+  //     if (e.target.checked === true) {
+  //       checkedArray.push()
+  //     }
+
   // }
+  const handleSearch = e => {
+    const newQueriesProducts = allProducts.filter(product =>
+      product.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    setQueriedProducts(newQueriesProducts)
+  }
 
-  const handleSearch = (e) => {
-    const newQueriesProducts = allProducts.filter((product) =>
-      product.name.toLowerCase().includes(e.target.value.toLowerCase())
-    );
-    setQueriedProducts(newQueriesProducts);
-  };
-  const handleSubmit = (e) => e.preventDefault();
+  const handleSubmit = e => e.preventDefault();
 
-  const productsJSX = queriedProducts.map((product, index) => (
-    <Product
-      _id={product._id}
-      key={index}
-      name={product.name}
-      photo={product.photos[0].imgURL}
-      price={product.price}
-    />
-  ));
 
+  const productsJSX = queriedProducts.map((product, index) =>
+    <Product _id={product._id} key={index} name={product.name.length > 25 && product.name.slice(0, 25)}
+      photo={product.photos ? product.photos[0] ? product.photos[0].imgURL : null : null} price={product.price} />
+  )
+  
   return (
     <Layout user={props.user}>
       <Search onSubmit={handleSubmit} onChange={handleSearch} />
-      {/* <Checkbox onClick={handleClick} setChecked={ }/> */}
-      <div className="products">{productsJSX}</div>
+      <Checkbox onChange={handleChange} onSubmit={handleSubmit} setChecked={setChecked} isChecked={!isChecked} />
+      <div className="products">
+        {productsJSX}
+
+      </div>
     </Layout>
   );
 };
