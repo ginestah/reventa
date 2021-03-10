5b945a134c24deba937270bf638db8d5c1c3965a
ginestah <68362184+ginestah@users.noreply.github.com>
2021-03-10 15:01:26 -0500

    working, could use some tweaks

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -6,7 +6,11 @@ const Product = (props) => {
   return (
     <>
       <Link className="product" to={`/products/${props._id}`}>
-        <img className="product-image" src={props.photo} alt={props.name} />
+        <img
+          className="product-image"
+          src={props.photo}
+          alt="furniture listing"
+        />
         <div className="price">{`$${props.price}`}</div>
         <div className="product-name">{props.name}</div>
       </Link>
--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import "./Products.css";
 import Product from "../../components/Product/Product";
@@ -7,13 +6,10 @@ import Layout from "../../components/shared/Layout/Layout";
 import { getProducts } from "../../services/products";
 import Checkbox from "../../components/Checkbox/Checkbox";
 
-
 const Products = (props) => {
   const [allProducts, setAllProducts] = useState([]);
   const [queriedProducts, setQueriedProducts] = useState([]);
-  // const [isChecked, setChecked] = useState(false);
-  const [options, setOptions] = useState([])
-  const [selectedProduct, setSelectedProduct] = useState([])
+  const [options, setOptions] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -23,54 +19,27 @@ const Products = (props) => {
     };
     fetchProducts();
   }, []);
-
-
-
-  const uncheck = () => {
-  
-}
   const handleChange = (e) => {
-    console.log(e.action)
-    // setChecked(!isChecked)
-    // console.log(isChecked)
-    // console.log(checked)
-    // console.log(e.target.value)
-    const value = e.target.type === 'checkbox' && e.target.checked
-    console.log(value)
+    const value = e.target.type === "checkbox" && e.target.checked;
+    console.log(value);
     if (value) {
-
-      setOptions([...options, e.target.value])
-      console.log(options)
-      //   console.log(options)
-      // }
-      // else {
-      //   const remove = e.target.value
-      //   const indexofRemove = options.findIndex((option) => (option === remove))
-      //   console.log(indexofRemove)
-      //   options.splice(indexofRemove,1,remove)
-      //   setOptions([...options])
-      const newQueriesProducts = allProducts.filter(product =>
+      setOptions([...options, e.target.value]);
+      console.log(options);
+      const newQueriesProducts = allProducts.filter((product) =>
         product.name.toLowerCase().includes(e.target.value.toLowerCase())
-
-      )
-      setQueriedProducts(newQueriesProducts)
-
+      );
+      setQueriedProducts(newQueriesProducts);
     } else {
+      let findNum = options.findIndex((option) => option === e.target.value);
+      const removeOne = options[findNum];
 
-      let findNum = options.findIndex(option => option === e.target.value)
-      const removeOne = (options[findNum])
+      options.splice(findNum, 1, removeOne);
 
-      options.splice(findNum, 1, removeOne)
-
-      setQueriedProducts(allProducts)
+      setQueriedProducts(allProducts);
     }
 
-
-
-
     // setSelectedProduct([...selectedProduct,newQueriesProducts])
-  }
-
+  };
 
   //   const checkSubmit = (e) => {
   //     let checkedArray = [];
@@ -79,28 +48,41 @@ const Products = (props) => {
   //     }
 
   // }
-  const handleSearch = e => {
-    const newQueriesProducts = allProducts.filter(product =>
-      product.name.toLowerCase().includes(e.target.value.toLowerCase()))
-    setQueriedProducts(newQueriesProducts)
-  }
+  const handleSearch = (e) => {
+    const newQueriesProducts = allProducts.filter((product) =>
+      product.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setQueriedProducts(newQueriesProducts);
+  };
 
-  const handleSubmit = e => e.preventDefault();
+  const handleSubmit = (e) => e.preventDefault();
 
+  const productsJSX = queriedProducts.map((product, index) => (
+    <Product
+      _id={product._id}
+      key={index}
+      name={product.name.length > 25 && product.name.slice(0, 25)}
+      photo={
+        product.photos
+          ? product.photos[0]
+            ? product.photos[0].imgURL
+            : null
+          : null
+      }
+      price={product.price}
+    />
+  ));
 
-  const productsJSX = queriedProducts.map((product, index) =>
-    <Product _id={product._id} key={index} name={product.name.length > 25 && product.name.slice(0, 25)}
-      photo={product.photos ? product.photos[0] ? product.photos[0].imgURL : null : null} price={product.price} />
-  )
-  
   return (
     <Layout user={props.user}>
       <Search onSubmit={handleSubmit} onChange={handleSearch} />
-      <Checkbox onChange={handleChange} onSubmit={handleSubmit} setQueriedProducts={setQueriedProducts} allProducts={ allProducts}/>
-      <div className="products">
-        {productsJSX}
-
-      </div>
+      <Checkbox
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+        setQueriedProducts={setQueriedProducts}
+        allProducts={allProducts}
+      />
+      <div className="products">{productsJSX}</div>
     </Layout>
   );
 };
