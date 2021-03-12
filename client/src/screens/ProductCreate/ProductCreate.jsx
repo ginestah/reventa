fc60bc7863efc9c119aa0bab73cf856dd526e1af
Brittany Owen <owen.brittany@gmail.com>
2021-03-12 13:06:00 -0600

    ProductCreate styled

--- client/src/screens/ProductCreate/ProductCreate.jsx
+++ client/src/screens/ProductCreate/ProductCreate.jsx
@@ -74,117 +74,144 @@ const ProductCreate = (props) => {
 
   return (
     <Layout user={props.user}>
-      <form className="create-form" onSubmit={handleSubmit}>
-        <label>Product Name:</label>
-        <input
-          className="input-name"
-          placeholder="Name"
-          value={product.name}
-          name="name"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
+      <div className="add-container">
+        <form className="create-form" onSubmit={handleSubmit}>
+          <div className="details-container">
+            <div>
+              <label>Product Name:</label>
+              <input
+                className="input-name"
+                placeholder="Name"
+                value={product.name}
+                name="name"
+                required
+                autoFocus
+                onChange={handleChange}
+              />
+            </div>
 
-        {product.photos.length > 4 ? null : (
-          <>
-            <input
-              type="url"
-              name="photos"
-              id="images"
-              value={imageAdd}
-              onChange={(e) => setImageAdd(e.target.value)}
-            />
-            <button
-              className="photo-button"
-              type="button"
-              onClick={handleImage}
-            >
-              Add Image
-            </button>
-          </>
-        )}
+            <div>
+              <label>Price:</label>
+              <input
+                className="input-price"
+                placeholder="Price"
+                value={product.price}
+                name="price"
+                required
+                onChange={handleChange}
+              />
+            </div>
 
-        <div className="preview-images">{imageJSX}</div>
+            <div>
+              <label>Location: </label>
+              <input
+                className="input-location"
+                placeholder="Location"
+                value={product.location}
+                name="location"
+                required
+                onChange={handleChange}
+              />
+            </div>
 
-        {/* https://www.cluemediator.com/add-or-remove-input-fields-dynamically-with-reactjs */}
-        {/* <label>Product Photos:</label>
+            <div>
+              <label>Shipping:</label>
+              <select
+                className="input-shipping"
+                value={product.shipping}
+                name="shipping"
+                required
+                onChange={handleChange}
+              >
+                <option value="selected">Please select one</option>
+                <option value="true">
+                  Available, not included in listing price
+                </option>
+                <option value="false">Pick up only</option>
+              </select>
+            </div>
+
+            <div>
+              <label>Contact Info:</label>
+              <input
+                required
+                placeholder="Phone number or email address"
+                value={product.contactInfo}
+                name="contactInfo"
+                className="input-contact-info"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <label>Product Description:</label>
+              <textarea
+                className="textarea-description"
+                rows={10}
+                placeholder="Description"
+                value={product.description}
+                name="description"
+                required
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-photos">
+              <label>Photos:</label>
+              <div>
+              {product.photos.length > 4 ? null : (
+                <>
+                  <input
+                    type="url"
+                    name="photos"
+                    id="images"
+                    value={imageAdd}
+                    onChange={(e) => setImageAdd(e.target.value)}
+                    />
+                  <button
+                    className="photo-button"
+                    type="button"
+                    onClick={handleImage}
+                    >
+                    Add Image
+                  </button>
+                </>
+              )}
+              </div>
+              </div>
+          </div>
+            <div className="preview-images">{imageJSX}</div>
+
+          {/* https://www.cluemediator.com/add-or-remove-input-fields-dynamically-with-reactjs */}
+          {/* <label>Product Photos:</label>
         {imageAdd.map((x, i) => {
           return (
             <div>
-              <input
-                required
-                className="input-image-link"
-                name="photos"
-                placeholder="Image Link"
-                value={product.photos}
-                onChange={handleImage}
-              />
-              <div className="button-box">
-                {imageAdd.length !== 1 && (
-                  <button onClick={() => handleRemoveClick(i)}>Remove</button>
+            <input
+            required
+            className="input-image-link"
+            name="photos"
+            placeholder="Image Link"
+            value={product.photos}
+            onChange={handleImage}
+            />
+            <div className="button-box">
+            {imageAdd.length !== 1 && (
+              <button onClick={() => handleRemoveClick(i)}>Remove</button>
+              )}
+              {imageAdd.length < 5 && imageAdd.length - 1 === i && (
+                <button onClick={handleAddClick}>Add</button>
                 )}
-                {imageAdd.length < 5 && imageAdd.length - 1 === i && (
-                  <button onClick={handleAddClick}>Add</button>
-                )}
-              </div>
-            </div>
-          );
-        })} */}
+                </div>
+                </div>
+                );
+              })} */}
 
-        <label>Product Description:</label>
-        <textarea
-          className="textarea-description"
-          rows={10}
-          placeholder="Description"
-          value={product.description}
-          name="description"
-          required
-          onChange={handleChange}
-        />
-        <label>Price: $</label>
-        <input
-          className="input-price"
-          placeholder="Price"
-          value={product.price}
-          name="price"
-          required
-          onChange={handleChange}
-        />
-        <label>Location: </label>
-        <input
-          className="input-location"
-          placeholder="Location"
-          value={product.location}
-          name="location"
-          required
-          onChange={handleChange}
-        />
-        <label>Shipping:</label>
-        <select
-          className="input-shipping"
-          value={product.shipping}
-          name="shipping"
-          required
-          onChange={handleChange}
-        >
-          <option value="selected">Please select one</option>
-          <option value="true">Available, not included in listing price</option>
-          <option value="false">Pick up only</option>
-        </select>
-        <label>Seller's Contact Info:</label>
-        <input
-          required
-          placeholder="Phone number or email address"
-          value={product.contactInfo}
-          name="contactInfo"
-          className="input-contact-info"
-          onChange={handleChange}
-        />
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
