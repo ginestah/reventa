dac0e8b6abfb74a02d231b736609e96401448fa1
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-03-15 16:25:25 -0400

    updated wishlist remove button

--- client/src/components/WishList/Wishlist.jsx
+++ client/src/components/WishList/Wishlist.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import './WishList.css'
 import { Link } from "react-router-dom"
 
-export const WishList = ({ name, price, index, photos, onClick }) => {
+export const WishList = ({ name, price, id, photos, onClick }) => {
   return (
     <div className="wish-container">
-      <Link to={`/products/${index}`}>
+      <Link to={`/products/${id}`}>
         <img className="wish-item-photo" src={photos} alt={name} />
       </Link>
       <div className="wish-item-name" >{name}</div>
       <div className="wish-item-price">${price}</div>
-      <button className="button" type="submit" value={name} name={index} onClick={onClick}>Remove</button>
+      <button className="button" type="submit" value={id} onClick={(e)=>onClick(e)}>Remove</button>
     </div >
   );
 };
