f37fbccd1c33f3f46496c46dd442a4a06f9f4664
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-03-15 12:40:23 -0400

    wish-list

--- client/src/screens/WishList/WishLists.jsx
+++ client/src/screens/WishList/WishLists.jsx
@@ -47,9 +47,9 @@ const WishLists = (props) => {
 
   return (
     <Layout user={props.user}>
-      <h3>My Wish Lists</h3>
+      <h3>My Wish List</h3>
       {
-        cart.length !== 0 ? <div className="wish-lists">{cartItems}</div> : <h3>My Wish List is Empty</h3> 
+        cart.length !== 0 ? <div className="wish-list">{cartItems}</div> : <h3>My Wish List is Empty</h3> 
       }
     </Layout>
   );
