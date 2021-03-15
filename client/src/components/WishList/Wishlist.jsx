43ce253710556cf79c67933357dcbd5e62c44d19
Brittany Owen <owen.brittany@gmail.com>
2021-03-14 23:06:08 -0500

    mobile media queries for wishlist screen and listings screen

--- client/src/components/WishList/Wishlist.jsx
+++ client/src/components/WishList/Wishlist.jsx
@@ -4,10 +4,12 @@ import './Wishlist.css'
 export const Wishlist = ({ name, price, index, photos, onClick }) => {
   return (
     <div className="wish-container">
+      <img className="wish-item-photo" src={photos} alt={name} />
+      <div>
       <div className="wish-item-name">{name}</div>
       <div className="wish-item-price">${price}</div>
-      <img className="wish-item-photo" src={photos} alt={name} />
       <input className="button" type="submit" value="Remove" name={index} onClick={onClick} />
+      </div>
     </div>
   );
 };
